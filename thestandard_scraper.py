1aa15b05a3ae22e26c3e5d5cb72b6828361b5489
SirDukey <SirDukey@bitbucket.org>
2018-07-13 22:00:45 +0200

    updated

--- thestandard_scraper.py
+++ thestandard_scraper.py
@@ -3,36 +3,32 @@ from time import sleep
 from PIL import Image
 from datetime import datetime
 import os
-import shutil
+from shutil import move
 import sys
 from zipfile import ZipFile
+from os import listdir, unlink, makedirs
+from os.path import basename
 
 
 def start_scrape(url, pages):
     start_time = str(datetime.now().strftime('%d-%m-%Y__%H:%M:%S'))
     down_dir = '/Novus_flask/downloaded/' + start_time
-    os.makedirs(down_dir)
+    makedirs(down_dir)
 
-    print()
-    print('start time:', start_time)
     yield 'start time: ' + start_time + '<br/>\n'
-    print('starting browser')
     yield 'starting browser<br/>\n'
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--log-level=3')
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')  # required when running as root user
+    chrome_options.add_argument('--no-sandbox')
     browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver',
                                chrome_options=chrome_options)
 
-    print('scraping url:', url)
     yield 'scraping url: ' + url + '<br/>\n'
-    print('attempting to scrape {} pages'.format(pages))
     yield 'attempting to scrape {} pages<br/>\n'.format(pages)
     browser.get(url)
     browser.set_window_size(2000, 2000)
 
-    print('logging in')
     yield 'logging in<br/>\n'
     username = browser.find_element_by_name('ctl00$ContentPlaceHolder1$txtEmailAddress')
     username.send_keys('elriza@novusgroup.co.za')
@@ -41,7 +37,6 @@ def start_scrape(url, pages):
 
     submit = browser.find_element_by_xpath('//*[@id="ContentPlaceHolder1_btnSubmit"]')
     submit.click()
-    print('login successful')
     yield 'login successful<br/>\n'
 
     # iterate over the total number of pages and capture
@@ -49,23 +44,18 @@ def start_scrape(url, pages):
 
     for page in range(0, int(pages)):
         if page % 2 == 0:
-            print('browsing to', url + '/' + str(page))
             yield 'browsing to ' + url + '/' + str(page) + '<br/>\n'
             browser.get(url + '/' + str(page))
             browser.set_window_size(4000, 2000)
             sleep(5)
 
-            print('rendering page')
             yield 'rendering page<br/>\n'
             try:
-                #elem = browser.find_element_by_css_selector('#frmBody')
-                #elem = browser.find_element_by_id('frmBody')
                 elem = browser.find_element_by_xpath('//*[@id="landscape"]')
                 elem.click()
                 elem.click()
                 sleep(5)
             except Exception as e:
-                print(str(e))
                 yield str(e)
                 sys.exit('fatal error')
 
@@ -77,12 +67,11 @@ def start_scrape(url, pages):
             im.thumbnail((4000, 4000))
             im = im.convert('RGB')
             im.save('/Novus_flask/downloaded/{}/page{}.jpeg'.format(start_time, page))
-            os.unlink('/Novus_flask/downloaded/{}/page{}.png'.format(start_time, page))
+            unlink('/Novus_flask/downloaded/{}/page{}.png'.format(start_time, page))
 
             original = Image.open('/Novus_flask/downloaded/{}/page{}.jpeg'.format(start_time, page))
 
             # Crop off black edges
-            width, height = im.size
             left = 0
             top = 0
             right = 3200
@@ -90,7 +79,6 @@ def start_scrape(url, pages):
             cropped_example = original.crop((left, top, right, bottom))
 
             # Get the left page
-            width_A, height_A = cropped_example.size
             left__a = 0
             top__a = 0
             right__a = 1600
@@ -99,14 +87,12 @@ def start_scrape(url, pages):
             cropped_example__a.save('/Novus_flask/downloaded/{}/page{}.jpeg'.format(start_time, page))
 
             # Get the right page
-            width_B, height_B = cropped_example.size
             left__b = 1600
             top__b = 0
             right__b = 3200
             bottom__b = 1960
             cropped_example__b = original.crop((left__b, top__b, right__b, bottom__b))
             cropped_example__b.save('/Novus_flask/downloaded/{}/page{}.jpeg'.format(start_time, page + 1))
-            print('saving to file: page{}.jpeg & page{}.jpeg'.format(page, page + 1))
             yield 'saving to file: page{}.jpeg & page{}.jpeg'.format(page, page + 1) + '<br/>\n'
 
     # close browser only after iteration of all pages is complete
@@ -114,33 +100,26 @@ def start_scrape(url, pages):
 
     # rename first page
     if 'page0.jpeg' in os.listdir('/Novus_flask/downloaded/{}'.format(start_time)):
-        os.unlink('/Novus_flask/downloaded/{}/page1.jpeg'.format(start_time))
-        shutil.move('/Novus_flask/downloaded/{}/page0.jpeg'.format(start_time), '/Novus_flask/downloaded/{}/page1.jpeg'.format(start_time))
+        unlink('/Novus_flask/downloaded/{}/page1.jpeg'.format(start_time))
+        move('/Novus_flask/downloaded/{}/page0.jpeg'.format(start_time),
+             '/Novus_flask/downloaded/{}/page1.jpeg'.format(start_time))
 
     # delete last page if page number is odd (black image)
     if pages % 2 != 0:
-        print('removing last blank page{}.jpeg'.format(pages))
         yield 'removing last blank page{}.jpeg'.format(pages) + '<br/>\n'
-        os.unlink('/Novus_flask/downloaded/{}/page{}.jpeg'.format(start_time, pages))
-
-
-    print()
-    print('complete')
-    print()
+        unlink('/Novus_flask/downloaded/{}/page{}.jpeg'.format(start_time, pages))
 
     # post start/end time for analysis
     end_time = str(datetime.now().strftime('%d-%m-%Y__%H:%M:%S'))
-    print('start time:', start_time)
-    print('end time:', end_time)
     yield 'end time:' + end_time + '<br/>\n'
     
     # TODO: zip the directory 
-    '''
-    with ZipFile(down_dir + '.zip', mode='a') as zf:
-        for down_file in os.listdir(down_dir):
-            zf.write(os.listdir(down_file, os.basename(down_file)))
+
+    working_dir = '/Novus_flask/downloaded/'
+    with ZipFile(start_time + '.zip', mode='a') as zf:
+        for f in listdir(working_dir):
+            zf.write(working_dir + f, basename(f))
     yield 'file zipped'
-    '''
 
     yield '<br/>\n'
     yield 'complete'

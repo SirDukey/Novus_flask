a3e07c6153869cfe2fc29729253ae6cc20e27136
SirDukey <SirDukey@bitbucket.org>
2018-07-27 14:22:07 +0200

    added multiprocessing

--- pdf2jpg.py
+++ pdf2jpg.py
@@ -2,6 +2,8 @@ from subprocess import check_call
 from os.path import splitext, basename
 from os import unlink, listdir
 from zipfile import ZipFile
+from datetime import datetime
+from multiprocessing import Pool
 
 
 working_dir = '/Novus_flask/uploaded/'
@@ -10,8 +12,8 @@ original_zipfile = '/Novus_flask/zip/converted.zip'
 
 def convert2pdf(pdf):
     jpg = splitext(pdf)[0] + ".jpg"
-
-    check_call(["convert", "-quality", "100%", "-density", "600", pdf, jpg])
+    with Pool(processes=20) as pool:
+        pool.map(check_call(["convert", "-quality", "100%", "-density", "600", pdf, jpg]))
     unlink(pdf)
     if original_zipfile:
         unlink(original_zipfile)
